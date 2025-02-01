3deab9e6fc32dfd1717f8b3199f43a4ba3de4b29
iliya kostadinov <iliakostadinov02@gmail.com>
2025-02-01 13:54:39 +0200

    update

--- uni projecto/GameStore/Core/Interfaces/IGameRepository.cs
+++ uni projecto/GameStore/Core/Interfaces/IGameRepository.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GameStore.Core.Models;
 
-namespace uni_projecto.GameStore.Core.Interfaces
+namespace GameStore.Core.Interfaces
 {
-    internal interface IGameRepository
+    public interface IGameRepository
     {
+        Task<Game> AddGame(Game game);
+        Task<IEnumerable<Game>> GetAllGames();
     }
 }
-;
